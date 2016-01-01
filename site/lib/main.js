7b8a7b8c0ff0eb66865d2b7d9f4295cf2148153d
hidden0 <thornethegreat@gmail.com>
2016-01-01 12:41:37 -0800

    More code templating

--- site/lib/main.js
+++ site/lib/main.js
@@ -1,6 +1,5 @@
 // Create the canvas
 var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
 
 // Global vars
 var windowWidth = window.innerWidth
@@ -14,3 +13,35 @@ var windowHeight = window.innerHeight
 canvas.width = windowWidth;
 canvas.height = windowHeight;
 document.body.appendChild(canvas);
+
+var Scorch = { }; // game obj
+var FPS=30; // framerate
+
+// Update
+Scorch.Update = function () {
+	windowWidth = window.innerWidth
+	|| document.documentElement.clientWidth
+	|| document.body.clientWidth;
+	windowHeight = window.innerHeight
+	|| document.documentElement.clientHeight
+	|| document.body.clientHeight;
+};
+// Render
+Scorch.Render = function () {
+	Scorch.ctx.fillStyle = "#000000";
+	Scorch.ctx.fillRect(0, 0, windowWidth, windowHeight);
+};
+// Game Loop
+Scorch.Game = function () {
+
+};
+// Init
+Scorch.Init = function () {
+	Scorch.ctx = canvas.getContext("2d");
+	setInterval(function() {
+      Scorch.Update();
+      Scorch.Render();
+    }, 1000/FPS);
+};
+
+Scorch.Init();
